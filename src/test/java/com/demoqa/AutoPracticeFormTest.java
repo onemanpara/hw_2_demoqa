c55cd50f69d2e7476f2f5a9e0e1e370e5da1b405
onemanpara <onemanpara@gmail.com>
2022-09-16 18:03:41 +0500

    replaced some selectors, added new validate

--- src/test/java/com/demoqa/AutoPracticeFormTest.java
+++ src/test/java/com/demoqa/AutoPracticeFormTest.java
@@ -23,34 +23,43 @@ public class AutoPracticeFormTest {
     String name = "Mikhail";
     String surname = "Nesterov";
     String email = "onemanpara@gmail.com";
+    String number = "9022541115";
 
     @Test
-    void FormTest() {
+    void formTest() {
         open("/automation-practice-form");
         $x("//input[@id='firstName']").setValue(name);
         $x("//input[@id='lastName']").setValue(surname);
         $x("//input[@id='userEmail']").setValue(email);
         $x("//label[contains(text(), 'Male')]").click();
-        $x("//input[@id='userNumber']").setValue("9022541115");
+        $x("//input[@id='userNumber']").setValue(number);
         $x("//input[@id='dateOfBirthInput']").click();
         $x("//select[@class='react-datepicker__month-select']").selectOption("August");
         $x("//select[@class='react-datepicker__year-select']").selectOption("1998");
         $x("//div[@class='react-datepicker__month-container']//div[@aria-label='Choose Wednesday, August 26th, 1998']").click();
         $x("//div[@id='subjectsContainer']//input").setValue("Maths").pressEnter();
-        $x("//label[@for='hobbies-checkbox-1']").click();
-        $x("//label[@for='hobbies-checkbox-2']").click();
-        $x("//label[@for='hobbies-checkbox-3']").click();
+        $x("//div[@id='hobbiesWrapper']//label[contains(text(), 'Sports')]").click();
+        $x("//div[@id='hobbiesWrapper']//label[contains(text(), 'Reading')]").click();
+        $x("//div[@id='hobbiesWrapper']//label[contains(text(), 'Music')]").click();
         $x("//input[@id='uploadPicture']").uploadFile(file);
         $x("//textarea[@placeholder='Current Address']").setValue("Test's Street");
         $x("//div[@class=' css-tlfecz-indicatorContainer']").scrollIntoView(true);
         $x("//div[@class=' css-tlfecz-indicatorContainer']").click();
-        $x("//div[@id='react-select-3-option-2']").doubleClick();
+        $x("//div[@id='state']//div[contains(text(), 'Haryana')]").doubleClick();
         $x("//div[@id='city']").click();
-        $x("//div[@id='react-select-4-option-0']").click();
+        $x("//div[@id='city']//div[contains(text(), 'Karnal')]").click();
         $x("//button[@id='submit']").click();
 
         $x("//div[@class='table-responsive']").shouldHave(Condition.text(name));
         $x("//div[@class='table-responsive']").shouldHave(Condition.text(surname));
         $x("//div[@class='table-responsive']").shouldHave(Condition.text(email));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text(number));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("26 August,1998"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Male"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Maths"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Sports, Reading, Music"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("test.jpg"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Test's Street"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Haryana Karnal"));
     }
 }
